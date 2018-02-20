8352248d1b59d54c49237b55e38afe217f129f94
BIT <BIT@DESKTOP-RS6IVTV>
2018-02-20 16:26:10 +0900

    prob4

--- src/prob4/Prob4.java
+++ src/prob4/Prob4.java
@@ -5,7 +5,28 @@ public class Prob4 {
 		char[] c1 = reverse( "Hello World" );
 		printCharArray( c1 );
 		
+		System.out.println();
 		char[] c2 = reverse( "Java Programming!" );
 		printCharArray( c2 );
+	}
+
+	private static char[] reverse(String string) {
+		char[] chars=string.toCharArray();
+		
+		for(int i=0; i<chars.length/2; i++) {
+			if(i> (chars.length-1)-i)break;
+			char tmp=chars[i];
+			chars[i]=chars[(chars.length-1)-i];
+			chars[(chars.length-1)-i]=tmp;
+		}
+		
+		
+		return chars;
+	}
+
+	public static void printCharArray(char[] source) {
+		for (char c : source) {
+			System.out.print(c);
+		}
 	}
 }
