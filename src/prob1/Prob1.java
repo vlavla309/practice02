eae187f50242ffa6d9ccee5deb71b5332a9aa76b
BIT <BIT@DESKTOP-RS6IVTV>
2018-02-20 16:10:35 +0900

    prob1

--- src/prob1/Prob1.java
+++ src/prob1/Prob1.java
@@ -5,13 +5,22 @@ import java.util.Scanner;
 public class Prob1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
-		
-		
+
 		System.out.print( "금액:" );
 		int money = scanner.nextInt();
-		
 
-		
+		int[] moneys= {50000, 10000, 5000, 1000,
+				500, 100, 50, 10, 5, 1};
+
+		for (int i : moneys) {
+			int count=0;
+			if(money>i) {
+				count=money/i;
+				money%=i;
+			}
+			System.out.println(i+"원 : "+count+"개");
+		}
+
 		scanner.close();
- 	}
+	}
 }
